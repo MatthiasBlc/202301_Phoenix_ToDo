2c3858bc636396d688323f2d1db9fca1a16ba4b5
MatthiasBlc <mat.bouloc@gmail.com>
2023-01-20 14:32:06 +0100

    count_tasks correction

--- lib/phoenix_todo_web/views/task_view.ex
+++ lib/phoenix_todo_web/views/task_view.ex
@@ -1,7 +1,7 @@
 defmodule PhoenixTodoWeb.TaskView do
   use PhoenixTodoWeb, :view
 
-  def count_task(completed, tasks) do
+  def count_tasks(completed, tasks) do
     tasks
     |> Enum.count(fn task -> task.completed == completed end)
   end
--- test/phoenix_todo_web/views/task_view_test.exs
+++ test/phoenix_todo_web/views/task_view_test.exs
@@ -3,23 +3,23 @@ defmodule PhoenixTodoWeb.TaskViewTest do
   import PhoenixTodo.TasksFixtures
   alias PhoenixTodoWeb.TaskView
 
-  test "count_task/2 returns count of tasks where completed" do
+  test "count_tasks/2 returns count of tasks where completed" do
     tasks = [
       %{text: "first", completed: true},
       %{text: "first", completed: false},
       %{text: "first", completed: false}
     ]
 
-    assert 1 = TaskView.count_task(true, tasks)
+    assert 1 = TaskView.count_tasks(true, tasks)
   end
 
-  test "count_task/2 returns count of tasks where not completed" do
+  test "count_tasks/2 returns count of tasks where not completed" do
     tasks = [
       %{text: "first", completed: true},
       %{text: "first", completed: false},
       %{text: "first", completed: false}
     ]
 
-    assert 2 = TaskView.count_task(false, tasks)
+    assert 2 = TaskView.count_tasks(false, tasks)
   end
 end
